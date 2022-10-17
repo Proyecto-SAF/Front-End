88618442086c816e86f034c6d8db224036c5952b
nhn25 <nhnkpo_14@outlook.es>
2022-10-17 09:45:32 -0300

    front end

---
+++ src/views/examples/HomePage.js
@@ -0,0 +1,102 @@
+import React, { useState, useRef, useMemo, useCallback } from 'react';
+
+
+// reactstrap components
+import {
+  Container,
+  Collapse,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav
+} from "reactstrap";
+
+
+
+// core components
+import HomePageHeader from "components/Headers/HomePageHeader";
+import CarouselSection from "views/index-sections/Carousel";
+import DefaultFooter from "components/Footers/DefaultFooter.js";
+import {Label, FormGroup} from "reactstrap";
+
+
+function HomePage() {
+  const [collapseOpen, setCollapseOpen] = React.useState(false);
+  const [firstFocus, setFirstFocus] = React.useState(false);
+  const [lastFocus, setLastFocus] = React.useState(false);
+  
+  
+  React.useEffect(() => {
+    document.body.classList.add("Home-page");
+    document.body.classList.add("sidebar-collapse");
+    document.documentElement.classList.remove("nav-open");
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+   
+    
+  }, []);
+  return (
+    <>
+      <Navbar className="bg-info" expand="lg">
+              <Container>
+                <div className="navbar-translate">
+                  <NavbarBrand
+                    href="#pablo"
+                    onClick={(e) => e.preventDefault()}
+                  >
+                    Info Color
+                  </NavbarBrand>
+                  <button
+                    onClick={() => {
+                      document.documentElement.classList.toggle("nav-open");
+                      setCollapseOpen(!collapseOpen);
+                    }}
+                    aria-expanded={collapseOpen}
+                    className="navbar-toggler"
+                    type="button"
+                  >
+                    <span className="navbar-toggler-bar bar1"></span>
+                    <span className="navbar-toggler-bar bar2"></span>
+                    <span className="navbar-toggler-bar bar3"></span>
+                  </button>
+                </div>
+                <Collapse isOpen={collapseOpen} navbar>
+                  <Nav className="ml-auto" navbar>
+                    <NavItem className="active">
+                      <NavLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        <p>Discover</p>
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        <p>Profile</p>
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        <p>Settings</p>
+                      </NavLink>
+                    </NavItem>
+                  </Nav>
+                </Collapse>
+              </Container>
+            </Navbar>
+      <div className="wrapper">
+        <HomePageHeader />
+        <DefaultFooter />
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
